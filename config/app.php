e6bf9b5b594a8de6cec02fe8609c40ab5c5e3489
JiaLe <jialeee0511@gmail.com>
2023-08-28 11:09:55 +0800

    Changed to UTC timezone

--- config/app.php
+++ config/app.php
@@ -70,7 +70,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'Asia/Kuala_Lumpur'),
+    'timezone' => 'UTC',
 
     /*
     |--------------------------------------------------------------------------
